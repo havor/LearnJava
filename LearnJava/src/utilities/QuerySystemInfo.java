756af9bbfb2aa22324ae7a829297c1bc1d1e2a74
tanhh <havor.tam@gmail.com>
2015-10-21 19:41:38 -0700

    Add a class: utilities.QuerySystemInfo

---
+++ LearnJava/src/utilities/QuerySystemInfo.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class QuerySystemInfo {
+	public static void main(String args[]){
+	    System.out.println( System.getProperty("os.name") );  
+	    System.out.println( System.getProperty("os.version") );  
+	    System.out.println( System.getProperty("os.arch") );  
+		
+	}
+
+}
